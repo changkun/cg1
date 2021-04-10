c964b66dadcb1e94427fa188fc7a829dfad6134c
Changkun Ou <hi@changkun.de>
2021-04-10 18:05:41 +0200

    demos: add 01-intro/1-setup

---
+++ demos/01-intro/1-setup/src/main.ts
@@ -0,0 +1,62 @@
+/**
+ * Copyright © 2021 LMU Munich Medieninformatik. All rights reserved.
+ * Created by Changkun Ou <https://changkun.de>.
+ *
+ * Use of this source code is governed by a GNU GLPv3 license that
+ * can be found in the LICENSE file.
+ */
+
+import './others';
+
+// Create an input box
+const input = document.createElement('input');
+document.body.appendChild(input);
+
+// Create a button and append it to the document body
+const btn = document.createElement('button');
+btn.textContent = 'Hello Computer Graphics 1 using TypeScript?';
+document.body.appendChild(btn);
+
+// Listen on the click event of the button
+btn.addEventListener('click', () => {
+  // Get the string from the input box and
+  // pass it as argument to sayHello
+  const msg = input.value;
+  sayHello(msg);
+});
+
+/**
+ * sayHello will print a console log in the JavaScript console, and
+ * create three <p> elements repeatedly.
+ *
+ * @param msg is an input message
+ */
+function sayHello(msg: string) {
+  // Print a console log
+  console.log('Hello mimuc/cg1 from console!');
+  if (msg.length !== 0) {
+    console.log('Your input is: ' + msg);
+  } else {
+    console.log('No inputs!');
+  }
+
+  // Create a <p> element and append to the document body
+  for (let i = 0; i < 3; i++) {
+    const p = document.createElement('p');
+    if (msg.length !== 0) {
+      p.textContent = `Hello mimuc.de/cg1! Your input is: ${msg}`;
+    } else {
+      p.textContent = 'Hello mimuc.de/cg1! But you did not say anything.';
+    }
+    document.body.appendChild(p);
+  }
+}
+
+// Hidden Question:
+//
+// The above code is almost the same as how you did JavaScript, right?
+// Except the `sayHello` function:
+//
+//   function sayHello(msg: string)
+//
+// Can you tell what will happen if the argument of sayHello is not a string?
---
+++ demos/01-intro/1-setup/src/others.ts
@@ -0,0 +1,80 @@
+/**
+ * Copyright © 2021 LMU Munich Medieninformatik. All rights reserved.
+ * Created by Changkun Ou <https://changkun.de>.
+ *
+ * Use of this source code is governed by a GNU GLPv3 license that
+ * can be found in the LICENSE file.
+ */
+
+/**
+ * Class
+ */
+
+class MyClass {
+  p1: string;
+  p2: number;
+  pn: Object[];
+  constructor(p1: string, p2: number, ...pn: Object[]) {
+    this.p1 = p1;
+    this.p2 = p2;
+    this.pn = [...pn];
+  }
+  f() {
+    console.log(this.p1, this.p2, this.pn);
+  }
+}
+const m = new MyClass('1', 2, 1, 2);
+m.f(); // '1', 2, [1, 2]
+
+/**
+ * Interface
+ */
+
+interface User {
+  name: string;
+  id: number;
+}
+
+class UserAccount {
+  name: string;
+  id: number;
+
+  constructor(name: string, id: number) {
+    this.name = name;
+    this.id = id;
+  }
+}
+
+const user: User = new UserAccount('Murphy', 1);
+console.log(user);
+
+/**
+ * Data types
+ */
+
+const b = false;
+const n = 3.1415;
+const s = 'Hello CG1';
+const a: Array<number> = [1, 2, 3, 4];
+const o: object = {course: 'mimuc/cg1', year: 2021, difficulty: 'ultra-easy'};
+console.log(b, n, s, a, o);
+
+/**
+ * elements counts the number of elements of a given argument.
+ *
+ * @param s is either a string or an array of string
+ * @returns the number of character elements of the given string or array.
+ */
+function elements(s: string | string[]) {
+  if (typeof s === 'string') {
+    return s.length;
+  }
+  let sum = 0;
+  for (let i = 0; i < s.length; i++) {
+    sum += s[i].length;
+  }
+  return sum;
+}
+
+console.log(elements('Hello CG1!'));
+console.log(elements(['Hello CG1!', 'Awesome Computer Graphics']));
